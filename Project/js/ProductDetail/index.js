ff48453652e4455147022c1d6bb4b6a8bba52a9f
hoangnam1997 <hoangnam.11.1997@gmail.com>
2017-10-12 16:12:20 +0700

    upload product detail day 2. file index

--- Project/js/ProductDetail/index.js
+++ Project/js/ProductDetail/index.js
@@ -1,1 +1,11 @@
 // crete by Namnh
+var swiper = new Swiper('.swiper-container', {
+	nextButton:'.swiper-button-next',
+	prevButton: '.swiper-button-prev',
+	pagination: '.swiper-pagination',
+	slidesPerView: 5,
+	paginationClickable: true,
+	spaceBetween: 30,
+	freeMode: true
+});
+
