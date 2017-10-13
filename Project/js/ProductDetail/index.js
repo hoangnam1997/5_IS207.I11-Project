0d9155e17aaa817d6cf3e53a828a60b207c45635
hoangnam1997 <hoangnam.11.1997@gmail.com>
2017-10-13 14:36:23 +0700

    update product detail design (Done).

--- Project/js/ProductDetail/index.js
+++ Project/js/ProductDetail/index.js
@@ -1,9 +1,17 @@
 // crete by Namnh
 // hover image and change detail
-$(document).ready(function($) {
+// function swap img
+function onSwapImg($from,$to){
+	var $src = $($from).attr('src');
+	$($to).attr('src',$src);
+	$('.zoomContainer .zoomWindowContainer .zoomWindow').css({'background-image':'url('+$src+')'});
+}
+$(document).ready(function() {
+	// change img
 	$('.listImgPrduct .img-loader').on('mouseover',function(){
 		$('.listImgPrduct .img-loader').css('border', 'none');
 		$(this).css('border', '1px solid #7F7B7B');
+		onSwapImg($(this),$('#productImgItem'));
 	});
 	// involve product
 	var swiper = new Swiper('.swiper-container', {
