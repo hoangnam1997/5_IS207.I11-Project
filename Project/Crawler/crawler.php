d53a14a81bb82e7c630d3d2bd3293c1294e85d59
ruanzetao <ruanzetao@gmail.com>
2017-10-23 16:17:13 +0700

    commit

---
+++ Project/Crawler/crawler.php
@@ -0,0 +1,43 @@
+<?php
+require "simple_html_dom.php";
+
+class DataClothe{
+	public $price;
+	public $name;
+	public $pictureurl;	
+	function DataClothe($price,$name,$pictureurl){
+		$this->$price=$price;
+		$this->$name=$name;
+		$this->$pictureurl=$pictureurl;
+	}
+
+	function INSERTDB(){
+		$qr="INSERT INTO CLOTHES VALUES($price,$name;$pictureurl)";
+		// Nên kiểm tra $this->url đã có trong db chưa
+	}
+}
+
+for($t=1;$t<=1;$t++){
+	getDatabase($t);
+}
+
+
+	function getDatabase($page){
+	$html=file_get_html("http://zuhaus.vn/zu-design-pc150502.html?page=$page");
+	$ds=$html->find("ul.products li");
+
+	foreach ($ds as $sp) {
+	# code...
+	$price=$sp->find("span.price span",0);
+	echo $price;
+	echo "<br>";
+	$name=$sp->find("a",3)->innertext;
+	echo $name; 
+	echo "<br>";
+	$pictureurl=$sp->find("img",0)->src;
+	echo $pictureurl;
+	echo "<br>";
+	}	
+}
+
+?>
