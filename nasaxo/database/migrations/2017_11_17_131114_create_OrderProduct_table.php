9a73a145e109c4d18dd3ec7f709024fec2f17abd
ruanzetao <ruanzetao@gmail.com>
2017-11-19 00:15:37 +0700

    commitOrderProduct

--- nasaxo/database/migrations/2017_11_17_131114_create_OrderProduct_table.php
+++ nasaxo/database/migrations/2017_11_17_131114_create_OrderProduct_table.php
@@ -15,6 +15,18 @@ class CreateOrderProductTable extends Migration
     {
         Schema::create('OrderProduct', function (Blueprint $table) {
             $table->increments('id');
+            $table->primary('id');
+            $table->integer('ID_Order');
+            $table->foreign('ID_Order')->references('id')->on('Order');
+            $table->integer('ID_Product');
+            $table->foreign('ID_Product')->references('id')->on('Product');
+            $table->integer('ID_Size');
+            $table->foreign('ID_Size')->references('id')->on('Size');
+            $table->integer('ID_Color');
+            $table->foreign('ID_Color')->references('id')->on('Color');
+            $table->integer('Count');
+            $table->string('Description');
+            $table->boolean('IsDelete');
             $table->timestamps();
         });
     }
