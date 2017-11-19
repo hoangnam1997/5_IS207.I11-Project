fc7c81deca605c65e0b583cbbba4373733de2121
ruanzetao <ruanzetao@gmail.com>
2017-11-19 10:22:18 +0700

    cmUsersPicturenew

--- nasaxo/database/migrations/2017_11_19_031920_create_UsersPicture_table.php
+++ nasaxo/database/migrations/2017_11_19_031920_create_UsersPicture_table.php
@@ -16,6 +16,11 @@ class CreateUsersPictureTable extends Migration
         Schema::create('UsersPicture', function (Blueprint $table) {
             $table->increments('id');
             $table->primary('id');
+            $table->integer('ID_Users');
+            $table->foreign('ID_Users')->references('id')->on('Users');
+            $table->integer('ID_Picture');
+            $table->foreign('ID_Picture')->references('id')->on('Picture');
+            $table->boolean('IsDelete');
             $table->timestamps();
         });
     }
