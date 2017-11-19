7e480c500d6d3b4eaafec275eba804f71d02efaa
ruanzetao <ruanzetao@gmail.com>
2017-11-19 10:24:14 +0700

    cmUsers

--- nasaxo/database/migrations/2017_11_17_122656_create_Users_table.php
+++ nasaxo/database/migrations/2017_11_17_122656_create_Users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('Users', function (Blueprint $table) {
             $table->increments('id');
+            $table->primary('id');
             $table->string('Username');
             $table->string('Password');
             $table->string('Picture');
