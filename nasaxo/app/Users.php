db4cb6223f1002fa3d652eee7e84f82f8540efb6
ruanzetao <ruanzetao@gmail.com>
2017-11-22 18:19:21 +0700

    commitusers

---
+++ nasaxo/app/Users.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Users extends Model
+{
+    public $timestamps = true;
+    //
+    protected $table = 'users';
+    protected $fillable = ['id','Username','Password','Picture','Email','Description','IsDelete','remember_token'];
+   
+}
