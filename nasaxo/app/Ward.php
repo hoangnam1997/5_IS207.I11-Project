2eb0ac16d2d4005976f0242bae5cd72ecfc45716
ruanzetao <ruanzetao@gmail.com>
2017-11-22 20:22:42 +0700

    modelward

---
+++ nasaxo/app/Ward.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ward extends Model
+{
+    public $timestamps = true;
+    protected $table = 'ward';
+    protected $fillable = ['id','Name','Description','ID_District','IsDelete'];
+    
+}
