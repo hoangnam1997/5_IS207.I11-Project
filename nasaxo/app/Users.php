485337b813f12152597bf3e5594f21ac53150135
ruanzetao <ruanzetao@gmail.com>
2017-11-22 18:35:11 +0700

    modelrole

--- nasaxo/app/Users.php
+++ nasaxo/app/Users.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Users extends Model
 {
     public $timestamps = true;
-    //
     protected $table = 'users';
     protected $fillable = ['id','Username','Password','Picture','Email','Description','IsDelete','remember_token'];
    
